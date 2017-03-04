276bf3c52cbf59ca0b962c4f503b404e6d4b26ef
loclPI <loclPI@hirntod.com>
2017-03-04 11:36:26 +0000

    added Post Test for Alexa Service on Azure

--- AlexaPost/test-POST.py
+++ AlexaPost/test-POST.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python 
 
 from sense_hat import SenseHat
-# sense=SenseHat()
-
-# tets POST to Alexa WS from python: 
-
-# http://stit17.azurewebsites.net:80/api/alexa/lastquake?lastQuakeStrentgh=8
-
-# >>> payload = {'key1': 'value1', 'key2': 'value2'}
-# >>> r = requests.post("http://httpbin.org/post", data=payload)
-
 
 import requests
 import json 
-url = 'http://stit17.azurewebsites.net:80/api/alexa/lastquake'
-payload = {'lastQuakeStrentgh': '7'}
-#response = requests.post(url, data=payload)
+# note that the param value is encoded in url !
 url = 'http://stit17.azurewebsites.net:80/api/alexa/lastquake?lastQuakeStrentgh=9'
 response = requests.post(url)
 
