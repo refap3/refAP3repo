e8841853930e76e5ac669bff7410c30ca967559f
refap3 <refap3@gmail.com>
2017-03-06 11:33:08 +0100

    Update EQ-POST.py

--- AlexaPost/EQ-POST.py
+++ AlexaPost/EQ-POST.py
@@ -7,6 +7,8 @@ import requests
 import json 
 
 sense = SenseHat()
+green = (0, 255, 0)
+white = (255, 255, 255)
 
 def postEQ(quakstr):
     # note that the param value is encoded in url !
@@ -39,6 +41,10 @@ def loop():
         y = round(y, 2)
         z = round(z, 2)
         quak+=math.sqrt((0-x)**2+(0-y)**2+(1-z)**2)
+	quakled=quak/30 * 64 
+	pixels = [green if i < quakled else white for i in range(64)]
+	sense.set_pixels(pixels)
+	
         print("x=%s, y=%s, z=%s" % (x, y, z))
         time.sleep(0.1)
     quak=math.sqrt(quak)
