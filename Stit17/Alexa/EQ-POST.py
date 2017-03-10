69de61966e519109505c8b16117dd47507bedb67
loclPI <loclPI@hirntod.com>
2017-03-10 08:20:08 +0000

    added detection of active host

--- Stit17/Alexa/EQ-POST.py
+++ Stit17/Alexa/EQ-POST.py
@@ -6,6 +6,7 @@ import math
 import requests
 import json 
 from random import randint
+import platform
 try:
     # For Python 3.0 and later
     from urllib.request import urlopen
@@ -22,6 +23,25 @@ def randomsparkle(sense):
     g = randint(0, 255)
     b = randint(0, 255)
     sense.set_pixel(x, y, r, g, b)
+
+def questionm(sense):
+    X = [255, 0, 0]  # Red
+    O = [255, 255, 255]  # White
+
+    question_mark = [
+        O, O, O, X, X, O, O, O,
+        O, O, X, O, O, X, O, O,
+        O, O, O, O, O, X, O, O,
+        O, O, O, O, X, O, O, O,
+        O, O, O, X, O, O, O, O,
+        O, O, O, X, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, X, O, O, O, O
+    ]
+
+    sense.set_pixels(question_mark)
+
+
 
 def posteq(quakstr):
     # note that the param value is encoded in url !
@@ -69,7 +89,8 @@ def setup():
     time.sleep(0.1)
 
 def loop():
-    # DO FOREVER 
+    # DO FOREVER
+    MYNODE=platform.node()
     while True:
 
         # get the run time parameters ..... we do this HERE in order to respond w/o restarts
@@ -77,56 +98,72 @@ def loop():
         configBaseURL="http://stit17.azurewebsites.net:80/api/Configurations/"
         indexMINQUAKE=8
         indexMEASURETIME=9
+        indexACTIVENODE=13
         paramjson=get_jsonparsed_data(configBaseURL+str(indexMINQUAKE))
         LIMIT=int(paramjson["Value"]) # no quake below this !
         paramjson=get_jsonparsed_data(configBaseURL+str(indexMEASURETIME))
         MEASURE=int(paramjson["Value"]) #  # of secs to measure 
-    
-        # RANDOM Sparkle for 3 secs ...
-        t_end=time.time() + 3 # run for x secs
-        while time.time() < t_end:
-            randomsparkle(sense)
-            time.sleep(0.01)
+        paramjson=get_jsonparsed_data(configBaseURL+str(indexACTIVENODE))
+        ACTIVENODE=(paramjson["Value"]) #  # of secs to measure
 
-        # BLANK screen for 1 secs ...
-        t_end=time.time() + 1 # run for x secs
-        while time.time() < t_end:
+        if ACTIVENODE == MYNODE:
+
+            # RANDOM Sparkle for 3 secs ...
+            t_end=time.time() + 3 # run for x secs
+            while time.time() < t_end:
+                randomsparkle(sense)
+                time.sleep(0.01)
+
+
+            # BLANK screen for 1 secs ...
             pixels = [BLUE for i in range(64)]
             sense.set_pixels(pixels)
-        
-        # flash brief 
-        pixels = [WHITE for i in range(64)]
-        sense.set_pixels(pixels)
-        time.sleep(0.5)
+            time.sleep(1)
 
-        # GO 
-        pixels = [BLACK for i in range(64)]
-        sense.set_pixels(pixels)
-        quak=0.0
-        t_end=time.time() + MEASURE # run for x secs
-        while time.time() < t_end:
-            x, y, z = sense.get_accelerometer_raw().values()
-            x = round(x, 2)
-            y = round(y, 2)
-            z = round(z, 2)
-            quak+=math.sqrt((0-x)**2+(0-y)**2+(1-z)**2)
-            quakled=math.sqrt(quak)/30 * 64 
-            pixels = [RED if i < quakled else BLACK for i in range(64)]
+            # flash brief
+            pixels = [WHITE for i in range(64)]
             sense.set_pixels(pixels)
-            # print("x=%s, y=%s, z=%s" % (x, y, z))
-            time.sleep(0.1)
-        
-        quak=math.sqrt(quak)
-        quakstr=str(round(quak,1)).replace('.',',')
-        
-        if quak > LIMIT:
-            print "you quaked: " + quakstr
-            sense.show_message(str(int(round(quak,0))))
-            posteq(quakstr)
-            dweetEQ(quakstr)
+            time.sleep(0.5)
+
+            # GO
+            pixels = [BLACK for i in range(64)]
+            sense.set_pixels(pixels)
+            quak=0.0
+            t_end=time.time() + MEASURE # run for x secs
+            while time.time() < t_end:
+                x, y, z = sense.get_accelerometer_raw().values()
+                x = round(x, 2)
+                y = round(y, 2)
+                z = round(z, 2)
+                quak+=math.sqrt((0-x)**2+(0-y)**2+(1-z)**2)
+                quakled=math.sqrt(quak)/30 * 64
+                pixels = [RED if i < quakled else BLACK for i in range(64)]
+                sense.set_pixels(pixels)
+                # print("x=%s, y=%s, z=%s" % (x, y, z))
+                time.sleep(0.1)
+
+            quak=math.sqrt(quak)
+            quakstr=str(round(quak,1)).replace('.',',')
+
+            if quak > LIMIT:
+                print "you quaked: " + quakstr
+                sense.show_message(str(int(round(quak,0))))
+                posteq(quakstr)
+                dweetEQ(quakstr)
+            else:
+                print "NOPE only: " + quakstr
+                sense.show_message("NOPE: " +str(int(round(quak,0))))
         else:
-            print "NOPE only: " + quakstr
-            sense.show_message("NOPE: " +str(int(round(quak,0))))
+
+            # Question MARK  screen for x secs ...
+            xsecs=1.5
+            questionm(sense)
+            time.sleep(xsecs)
+            # BLANK screen for 1 secs ...
+            pixels = [BLUE for i in range(64)]
+            sense.set_pixels(pixels)
+            time.sleep(xsecs)
+
 
 
 # REGION INIT ...        
