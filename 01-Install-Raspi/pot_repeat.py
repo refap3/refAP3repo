5f4cf72f1a0bf79c1b0969cbebb3b607fc01f7cf
loclPI <loclPI@hirntod.com>
2017-04-24 16:00:27 +0000

    commented

--- 01-Install-Raspi/pot_repeat.py
+++ 01-Install-Raspi/pot_repeat.py
@@ -1,6 +1,10 @@
+#!/usr/bin/python
+
 # pot_repeat.py - continuously measure resistance of a potentiometer
 # (c) BotBook.com - Karvinen, Karvinen, Valtokari
+# from the book getting started with sensors
 
+# read and convert values between 0 and 1023 (10 bits)
 
 import botbook_mcp3002 as mcp	# <1>
 import time	# <2>
